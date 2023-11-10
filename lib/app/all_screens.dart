7c880a85cc716ac3e18f9bf061ffb225101af015
sanamrahimi <sanam.rahimi@gmail.com>
2023-11-10 20:53:53 +0500

    update screens

--- lib/app/all_screens.dart
+++ lib/app/all_screens.dart
@@ -1,7 +1,7 @@
 export 'package:flutter/material.dart';
-export 'package:login_signup_page/widgets/constants.dart';
+export 'package:login_signup_page/screens/components/constants.dart';
 export 'package:login_signup_page/screens/login_screen.dart';
-export 'package:login_signup_page/widgets/custom_header.dart';
+export 'package:login_signup_page/screens/components/custom_header.dart';
 export 'package:login_signup_page/screens/forget_password_screen.dart';
 export 'package:login_signup_page/screens/signup_screen.dart';
 
